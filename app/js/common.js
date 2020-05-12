dd1bd84d82d45b0583c9b950fc1ddeeb9c22421e
alex <alex_kobilianskiy@bigmir.net>
2020-05-12 17:20:16 +0300

    header complete

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,51 @@
 $(document).ready(function(){
+    /**
+     * mobile-mnu customization
+     */
+    var mmenu = $('#mobile-mnu');
+    var menuName = mmenu.data("name");
+    var menuDesc = mmenu.data("description");
+    var $mmenu = mmenu.mmenu({
+        navbars: [{
+            content: [ "<div class='mmenu-logo'><div class='name'>"+menuName+"</div><div class='description'>"+menuDesc+"</div></div>" ],
+            height: 3
+        }],
+        "pageScroll": true,
 
+        "navbar": {
+            "title" : "",
+        },
+        "extensions": [
+            "theme-dark",
+            "pagedim-black",
+            "position-front",
+            "fx-listitems-slide",
+        ],
+    }, {
+        offCanvas: {
+            pageSelector: "#page-wrapper"
+        },
+    });
+
+    var mmenuBtn = $(".mmenu-btn");
+    var API = $mmenu.data("mmenu");
+
+    mmenuBtn.click(function() {
+        API.open();
+        $(this).addClass('is-active')
+    });
+
+
+    API.bind( "close:start", function() {
+        setTimeout(function() {
+            mmenuBtn.removeClass( "is-active" );
+        }, 300);
+    });
+    /**
+     * end mobile-mnu customization
+     */
+    
+    
     //E-mail Ajax Send
     $("form").submit(function() { //Change
         var th = $(this);
