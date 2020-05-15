198e5c374d174d603279a199aed6297bbdd52730
alex <alex_kobilianskiy@bigmir.net>
2020-05-15 18:18:23 +0300

    sections categories and form complete (without clouds)

--- app/js/common.js
+++ app/js/common.js
@@ -46,6 +46,39 @@ $(document).ready(function(){
      */
 
 
+    $('img.svg').each(function(){
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+
+
 
     $('.intro-slider').owlCarousel({
         loop:true,
@@ -59,8 +92,23 @@ $(document).ready(function(){
         touchDrag: false,
         // autoplay: true,
     });
-    
-    
+
+
+    /** FORMS START*/
+    var uPhone = $('.user-phone');
+    uPhone.mask("+7 (999) 999-99-99",{autoclear: false});
+
+    uPhone.on('click', function (ele) {
+        var needelem = ele.target || event.srcElement;
+        needelem.setSelectionRange(4,4);
+        needelem.focus();
+    });
+
+    $.validate({
+        form : '.contact-form',
+        scrollToTopOnError: false
+    });
+
     //E-mail Ajax Send
     $("form").submit(function() { //Change
         var th = $(this);
@@ -74,4 +122,5 @@ $(document).ready(function(){
         });
         return false;
     });
+    /** FORMS END */
 });
--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,8 @@ gulp.task('scripts', function() {
 		'app/libs/mmenu/jquery.mmenu.all.js',
 		'app/libs/owl-carousel/owl.carousel.min.js',
 		'app/libs/owl-carousel/owl.carousel2.thumbs.min.js',
+		'app/libs/inputmask/jquery.maskedinput.min.js',
+		'app/libs/jquery-form-validator/form-validator/jquery.form-validator.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
